3230777c3c2fd19ffc555d638019f8e2bd0d910b
Aaron Bockover <abock@xamarin.com>
2018-08-17 16:04:12 -0400

    Keychain: import ManagedProtection from Mono

---
+++ src/Xamarin.Security.Keychain/Mono.Security.Cryptography/Locale.cs
@@ -0,0 +1,16 @@
+﻿//
+// Locale.cs
+//
+// Author:
+//   Aaron Bockover <abock@xamarin.com>
+//
+// Copyright 2017 Microsoft. All rights reserved.
+
+namespace Mono.Security.Cryptography
+{
+	static class Locale
+	{
+		public static string GetText (string message, params object [] args)
+			=> string.Format (message, args);
+	}
+}
